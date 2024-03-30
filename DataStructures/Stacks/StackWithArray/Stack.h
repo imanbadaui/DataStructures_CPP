b7460d26cc60cae84962e4f5f26e1009f0817b64
Iman Badaui <iman.badaui@gmail.com>
2024-03-30 02:43:25 +0200

    Create Stack.h

---
+++ DataStructures/Stacks/StackWithArray/Stack.h
@@ -0,0 +1,118 @@
+#ifndef STACK_H
+#define STACK_H
+
+template <typename T>
+class Stack
+{
+public:
+    Stack(const int max=100) : top{0}, max_size{max}
+    {
+        arr = new T[max_size];
+
+    }
+    bool is_full()
+    {
+        return (top == max_size);
+    }
+    bool is_empty() const
+    {
+
+        return (top == 0);
+    }
+
+    void  push (const T& element)
+    {
+        arr[top++] = element;
+    }
+
+    void  pop(T& element)
+    {
+        element = arr[top-1];
+        top--;
+
+    }
+
+    void stack_top(T& element)
+    {
+        element  = arr[top-1];
+
+    }
+
+    int length()
+    {
+        return top;
+
+    }
+    int size()
+    {
+        return max_size;
+    }
+
+    void clear()
+    {
+        top = 0;
+
+    }
+
+    void traverse (void (*func) (const T& stack_element))
+    {
+        for(int i{top-1}; i>=0 ; --i )
+        {
+            func(arr[i]);
+
+        }
+    }
+
+    Stack (const Stack<T>& otherStack)
+    {
+        arr = nullptr;
+        top = 0;
+        copy_stack(otherStack);
+    }
+
+    const Stack<T>& operator= (const Stack<T>& otherStack)
+    {
+        if(this != &otherStack)
+        {
+            copy_stack(otherStack);
+
+        }
+        return *this;
+    }
+
+    virtual ~Stack()
+    {
+        delete[] arr;
+
+    }
+
+private:
+    int top;
+    int max_size;
+    T *arr;
+
+    void copy_stack(const Stack<T>& otherStack)
+    {
+        if(!is_empty())
+        {
+            delete[] arr;
+
+        }
+
+        max_size = otherStack.max_size;
+        top = otherStack.top;
+        arr = new T [max_size];
+
+
+        for (int i {0}; i < top ; ++i)
+        {
+            arr[i] = otherStack.arr[i];
+
+        }
+    }
+
+
+
+};
+
+#endif // STACK_H
